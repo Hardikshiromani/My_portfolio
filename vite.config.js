7591c2976706ab918ad44ffcb31d3bd7ceb846ae
Hardikshiromani <hardikshiromani786@gmail.com>
2025-09-09 17:36:43 +0530

    modifed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
+   base: './', // 👈 Ensures relative paths for assets
+
   plugins: [react()],
    resolve: {
     alias: {
