c2a8fb4eab61257c562ddeae52bb5827bb250f13
Hardikshiromani <hardikshiromani786@gmail.com>
2025-09-10 17:18:05 +0530

    modified config files1

--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,12 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
+
+   // ⬇️ This prevents PostCSS from touching .js files
+  exclude: [
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.ts',
+    '**/*.tsx',
+  ],
 }
